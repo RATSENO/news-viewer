fbc1b2f720c8cee73e0667aae3942d3f8fbfcf16
RATSENO <onestar1014@gmail.com>
2022-07-22 11:04:25 +0900

    async await

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { useState } from "react";
 const App = () => {
   const [data, setData] = useState(null);
   
-  const onClick = () =>{
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response)=>{
-      setData(response.data);
-    });
-  };
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      setData(response.data);  
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
